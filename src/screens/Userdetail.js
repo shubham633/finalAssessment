61317aaa8c6c1de64b137a9ad2c1c0b0e2dfef7c
shubham <shubhamsahuamethi@gmail.com>
2020-08-24 12:16:35 +0530

    'commited'

--- src/screens/Userdetail.js
+++ src/screens/Userdetail.js
@@ -1,21 +1,70 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import {View,Text,TouchableOpacity,Image,Button,TextInput,StyleSheet} from 'react-native';
+import Realm from 'realm';
+import {Schema} from '../database/Schema';
+import {updateUser} from '../database/AllQuerySchema';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-const Userdetail = ({navigation}) =>{
-  return(
+let realm;
+const Userdetail = (props) => {
+  const [name, setName] = useState('');
+  const [phoneNo, setPhoneNo] = useState('');
+  const [editable, setEditable] = useState(false);
+
+  
+  var value;
+  var obj;
+  realm = new Realm({schema: [Schema]});
+    obj = realm.objects('Signup');
+    console.log(obj);
+    for(value of obj)
+    console.log('update'+value.username);
+  
+  return (
     <View>
-    <Text>
-    Userdetail
-    </Text>
-    <TouchableOpacity onPress={()=>navigation.navigate('Dashboard')}>
-    <Text>Dashboard</Text>
-    </TouchableOpacity>
+      <View style={{flexDirection: 'row'}}>
+        <Text style={{marginTop: 10}}>UserName: </Text>
+        <TextInput
+          style={styles.text}
+          placeholder={value.username}
+          value={name}
+          onChangeText={(text) => {
+            setName(text);}}
+          editable={editable}
+        />
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <Text style={{marginTop: 10}}>PhoneNo: </Text>
+        <TextInput
+          style={styles.text}
+          placeholder={value.phone_no}
+          value={phoneNo}
+          onChangeText={(text) => {
+            setPhoneNo(text);}}
+          editable={editable}
+        />
+      </View>
+      <Button
+        title="update"
+        onPress={() => {
+          const userData = {
+            username: username,
+            phone_no: phone_no,
+          };
+          updateUser(userData);
+        }}
+      />
     </View>
-
   );
-}
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create(
+    {
+        text:{
+            width:'60%',
+            fontWeight:'bold'
+        }, 
 
+    }
+)
 export default Userdetail;
